cb5baa5a42665ca21f0986735e39ce00531a542d
mwohl <molly.wohl@gmail.com>
2015-07-29 18:01:56 -0500

    added the basics of a bare-bones Flask app

--- ToppTrack/__init__.py
+++ ToppTrack/__init__.py
@@ -1,11 +1,5 @@
 from flask import Flask
-from flask.ext.mongoengine import MongoEngine
 
 app = Flask(__name__)
-app.config["MONGODB_SETTINGS"] = {'db':"ToppTrack"}
-app.config["SECRET_KEY"] = "bawe924nsli5sk35b2c912nks"
 
-db = MongoEngine(app)
-
-if __name__ == '__main__':
-	app.run()
+from app import views
---
+++ ToppTrack/views.py
@@ -0,0 +1,6 @@
+from app import app
+from flask import request, render_template, redirect, url_for
+
+@app.route('/', methods=['GET'])
+def index():
+	return render_template('home.html')
